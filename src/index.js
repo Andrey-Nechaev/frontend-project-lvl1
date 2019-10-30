49067eaa55756b9229dcbed580c1b8a0e7e4256b
AndreyGlyan <aglyan82@gmail.com>
2019-10-30 17:38:45 +0300

    typeError

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-/* #!/usr/bin/env node */
 import readlineSync from 'readline-sync';
 
-export default greeting = () => {
+export const greeting = () => {
 	console.log('Welcome to the Brain Games!!!');
-	/* const playerName = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${playerName}!`); */
+	const playerName = readlineSync.question('May I have your name? ');
+	console.log(`Hello, ${playerName}!`);
 };
  
