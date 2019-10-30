edbe443e1f571234a04c443f9c88497503d30d3c
AndreyGlyan <aglyan82@gmail.com>
2019-10-30 13:18:56 +0300

    syntaxError

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* #!/usr/bin/env node */
+import readlineSync from 'readline-sync';
+
+export default greeting = () => {
+	console.log('Welcome to the Brain Games!!!');
+	/* const playerName = readlineSync.question('May I have your name? ');
+	console.log(`Hello, ${playerName}!`); */
+};
+ 
